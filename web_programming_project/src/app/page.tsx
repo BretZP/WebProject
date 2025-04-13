84adec04517b355614fa781937861c01f1412597
Bret Zubieta <bzubietap@.gmail.com>
2025-04-12 21:09:10 -0400

    Testing

--- web_programming_project/src/app/page.tsx
+++ web_programming_project/src/app/page.tsx
@@ -4,12 +4,12 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <div className="min-h-screen bg-[var(--background)] text-[var(--text)]">
-    {/* Navbar stays outside of the image section */}
+   
     <Navbar />
 
-    {/* Splash / hero section */}
+ 
     <div className="relative h-[300px] w-full overflow-hidden">
-      {/* This is your image background */}
+
       <Image
         src="/MusTheory.jpg"
         alt="Sheet music background"
@@ -17,7 +17,7 @@ export default function Home() {
         className="object-cover opacity-40"
       />
 
-      {/* Overlayed text and search bar */}
+      
       <div className="relative z-10 flex flex-col items-center justify-center h-full">
         <h1 className="text-4xl font-bold">Scale Explorer</h1>
         <input
