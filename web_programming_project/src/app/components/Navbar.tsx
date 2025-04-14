3f9a798a2a88797fa0b4bfcd260071f0c1a353b2
Yuval Deshe <yuvaldeshe@gmail.com>
2025-04-14 18:51:58 -0400

    Readded Register/Login link to Navbar.

--- web_programming_project/src/app/components/Navbar.tsx
+++ web_programming_project/src/app/components/Navbar.tsx
@@ -2,13 +2,13 @@
 
 import React from "react";
 import "@/app/globals.css";
-import { HomeIcon } from "@heroicons/react/24/solid"; 
-import {useState} from 'react';
+import { HomeIcon } from "@heroicons/react/24/solid";
+import { useState } from 'react';
 import Button from "@/app/components/Button";
 
 
 interface NavbarProps {
-  onBooleanChange: (value:boolean)=>void;
+  onBooleanChange: (value: boolean) => void;
 }
 
 const Navbar = () => {
@@ -21,30 +21,30 @@ const Navbar = () => {
   }
 
   return (
-      <nav className="w-full flex justify-between items-center bg-accent-background rounded-lg px-6 py-4 fixed z-10">
-        <div className="text-lg font-semibold">
+    <nav className="w-full flex justify-between items-center bg-accent-background rounded-lg px-6 py-4 fixed z-10">
+      <div className="text-lg font-semibold">
 
-          <a href="/" className="color-text no-underline hover:opacity-80 transition-opacity">
+        <a href="/" className="color-text no-underline hover:opacity-80 transition-opacity">
           <HomeIcon className="h-6 w-6" />
-           
 
+
+        </a>
+      </div>
+
+      <ul className="flex list-none gap-6 m-0 p-0">
+        <li>
+          <a
+            href="/register"
+            className="text-sm text-foreground no-underline hover:opacity-80 transition-opacity font-[family-name:var(--font-geist-mono)]"
+            onClick={handleLogin}
+          >
+            Register / Login
           </a>
-        </div>
-        
-        <ul className="flex list-none gap-6 m-0 p-0">
-          <li>
-            {/* <a
-              href="/register"
-              className="text-sm text-foreground no-underline hover:opacity-80 transition-opacity font-[family-name:var(--font-geist-mono)]"
-              onClick={handleLogin}
-            >
-              Register / Login
-          </a> */}
-              <Button onClick={handleLogin}>{isLoggedIn ? "Login" : "Logout"}</Button>
-          </li>
-        </ul>
+          {/* <Button onClick={handleLogin}>{isLoggedIn ? "Login" : "Logout"}</Button> */}
+        </li>
+      </ul>
 
-      </nav>
+    </nav>
   );
 }
 
--- web_programming_project/src/app/login/page.tsx
+++ web_programming_project/src/app/login/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import Navbar from "@/app/components/Navbar";
 
 import LoginForm from "@/app/components/LoginForm";
 
@@ -12,15 +11,14 @@ const Login = () => {
     }
 
     const handleAddUser = (newUser:User) => {
-        console.log(`Username: ${newUser.username} | Password: ${newUser.password} added.
-            \nThis is a placeholder function for until we implement MongoDB connection.`);
+        console.log(`Username: ${newUser.username} | Password: ${newUser.password} logged in.
+            This is a placeholder function for until we implement MongoDB connection.`);
     }
 
     return (
         <div className="flex grow-0 shrink-1 basis-0 items-center justify-center w-full min-h-full">
-            <Navbar />
-            <div className="RegisterLogin"> {/*make this prettier for different resolutions */}
-                <h1 className="text-2xl dark:text-white light:text-black mb-8">Login</h1>
+            <div className="RegisterLogin">
+                <h1 className="text-2xl dark:text-white light:text-black mt-4 mb-8">Login</h1>
                 <p className="mb-8">Don't have an account? <a className="link underline" href="/register">Register</a></p>
                 <LoginForm onLogin={handleAddUser}/>
             </div>
