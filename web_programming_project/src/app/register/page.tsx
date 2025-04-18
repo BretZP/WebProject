bd298a5a3f4526bf806a97ae4cda9cc4c992a6c6
Yuval Deshe <yuvaldeshe@gmail.com>
2025-04-18 12:14:31 -0400

    Removed light theme option

--- web_programming_project/src/app/register/page.tsx
+++ web_programming_project/src/app/register/page.tsx
@@ -18,7 +18,7 @@ const Register = () => {
     return (
         <div className="flex grow-0 shrink-1 basis-0 items-center justify-center w-full min-h-full">
             <div className="RegisterLogin">
-                <h1 className="text-2xl dark:text-white light:text-black mt-4 mb-8">Register</h1>
+                <h1 className="text-2xl text-text mt-4 mb-8">Register</h1>
                 <p className="mb-8">Already have an account? <a className="link underline" href="/login">Login</a></p>
                 <RegisterForm onRegister={handleAddUser}/>
             </div>
